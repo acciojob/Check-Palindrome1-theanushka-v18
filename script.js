76e061ba2012e33572e1bb4ae760337d93226b59
Anushka Verma <93106932+theanushka-v18@users.noreply.github.com>
2023-02-07 11:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 function palindrome(str){
 
+var re = /[\W_]/g;
+var lowRegStr = str.toLowerCase().replace(re, '');
+var reverseStr = lowRegStr.split('').reverse().join(''); 
+return reverseStr === lowRegStr;
+
 }
 module.exports = palindrome
